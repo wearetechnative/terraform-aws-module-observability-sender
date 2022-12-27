51bd028e263997ef34db78a17222aaf8e0283b8d
Ronald <ronald@technative.nl>
2022-12-27 19:52:25 +0100

    TLDA-290: Extend at least the IAM role naming to prevent collisions when deploying in the same account over multiple regions.

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  lambda_cw_alarm_name     = "cloudwatch_alarm_creator"
-  lambda_payload_forwarder = "payload_forwarder"
+  lambda_cw_alarm_name     = "cloudwatch_alarm_creator_${data.aws_region.current.name}"
+  lambda_payload_forwarder = "payload_forwarder_${data.aws_region.current.name}"
 }
