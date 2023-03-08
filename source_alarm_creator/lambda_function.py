e33548def77f4704cb126b77112de28661ea43dd
Andrew <andrew@technative.nl>
2023-03-08 09:55:48 +0100

    Introduced DDGC changes to source alarm creator

--- source_alarm_creator/lambda_function.py
+++ source_alarm_creator/lambda_function.py
@@ -4,7 +4,8 @@ import os
 import logging
 import traceback
 import sys
-from actions import *
+import datetime
+from actions import Cwagent_alarms, AWS_EC2_Alarms, RDS_Alarms, DeleteAlarms
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -14,20 +15,28 @@ sns_arn = os.environ["SNS_ARN"]
 
 
 def lambda_handler(event, context):
+    print(event)
 
-    # Slep added to eleviate the API calls in order to not receive an API throttling call.
     try:
+        print("{}: Cwagent_alarms()".format(datetime.datetime.now()))
         Cwagent_alarms()
+        print("{}: AWS_EC2_Alarms()".format(datetime.datetime.now()))
         AWS_EC2_Alarms()
+        print("{}: RDS_Alarms()".format(datetime.datetime.now()))
         RDS_Alarms()
+        print("{}: DeleteAlarms()".format(datetime.datetime.now()))
         DeleteAlarms()
+        print("{}: Finished()".format(datetime.datetime.now()))
     except Exception as exp:
         exception_type, exception_value, exception_traceback = sys.exc_info()
-
+        traceback_string = traceback.format_exception(
+            exception_type, exception_value, exception_traceback
+        )
         err_msg = json.dumps(
             {
                 "errorType": exception_type.__name__,
                 "errorMessage": str(exception_value),
+                "stackTrace": traceback_string,
             }
         )
 
@@ -37,3 +46,5 @@ def lambda_handler(event, context):
             Message=json.dumps({"default": err_msg}),
             MessageStructure="json",
         )
+
+        raise
