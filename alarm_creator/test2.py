54f32a387bab4d522fdcc1129beac944e50edffa
jerpen80 <dra_penny@yahoo.com>
2024-02-15 16:47:22 +0100

    improvements

--- alarm_creator/test2.py
+++ alarm_creator/test2.py
@@ -12,7 +12,6 @@ ecsclient = boto3.client("ecs")
 with open('./alarms.json') as alarms_file:
     alarms = json.load(alarms_file)
 
-#print(alarms)
 def AWS_Alarms():
     for service in alarms:
         if service == "EC2":
@@ -27,11 +26,11 @@ def AWS_Alarms():
             for metrics in response["Metrics"]:
                 if metrics["MetricName"] == alarms[service][alarm]['MetricName']:
                     for dimensions in metrics["Dimensions"]:
-                        if dimensions["Name"] == alarms[service][alarm]['Dimensions']['Name']:
+                        if dimensions["Name"] == alarms[service][alarm]['Dimensions']:
                             for priority, threshold in zip(alarms[service][alarm]['AlarmThresholds']["priority"], alarms[service][alarm]['AlarmThresholds']["alarm_threshold"]):
                                 for instance in instances:
                                     CWclient.put_metric_alarm(
-                                        AlarmName=f"{instance}-{alarm} > {threshold}%",
+                                        AlarmName=f"{instance}-{alarm} {alarms[service][alarm]['Operatorsymbol']} {threshold}%",
                                         ComparisonOperator=alarms[service][alarm]['ComparisonOperator'],
                                         EvaluationPeriods=alarms[service][alarm]['EvaluationPeriods'],
                                         MetricName=alarms[service][alarm]['MetricName'],
@@ -42,7 +41,7 @@ def AWS_Alarms():
                                         ActionsEnabled=True,
                                         TreatMissingData=alarms[service][alarm]['TreatMissingData'],
                                         AlarmDescription=f"{priority}",
-                                        Dimensions=alarms[service][alarm]['Dimensions'],
+                                        Dimensions=[{"Name":  f"{dimensions["Name"]}", "Value": f"{dimensions['Value']}"},],
                                         Unit=alarms[service][alarm]['Unit'],
                                         Tags=[{"Key": "CreatedbyLambda", "Value": "True"}],
                                     )
@@ -70,21 +69,35 @@ def GetRunningDBInstances():
 
     return RunningDBInstances
 
+def GetRunningClusters():
+    get_running_clusters = ecsclient.list_clusters()
+    RunningClusters = get_running_clusters["clusterArns"]
+    RunningClusterNames = []
+    for clusters in RunningClusters:
+        RunningClusterNames.append(clusters.split('/')[1])
+
+    return RunningClusterNames
+
 def DeleteAlarms():
     get_alarm_info = CWclient.describe_alarms()
     RunningInstances = GetRunningInstances()
     RunningRDSInstances = GetRunningDBInstances()
+    RunningClusters = GetRunningClusters()
     
     # collect alarm metrics and compare alarm metric instanceId with instance id's in array. if the state reason is breaching and instance does not exist delete alarm.
     for metricalarm in get_alarm_info["MetricAlarms"]:
         instance_id = list(filter(lambda x: x["Name"] == "InstanceId", metricalarm["Dimensions"]))
         rds_instance_name = list(filter(lambda x: x["Name"] == "DBInstanceIdentifier", metricalarm["Dimensions"]))
+        cluster_name = list(filter(lambda x: x["Name"] == "ClusterName", metricalarm["Dimensions"]))
         
         if len(instance_id) == 1:
             if instance_id[0]["Value"] not in RunningInstances:
                 CWclient.delete_alarms(AlarmNames=[metricalarm["AlarmName"]])
         if len(rds_instance_name) == 1:
             if rds_instance_name[0]["Value"] not in RunningRDSInstances:
+                CWclient.delete_alarms(AlarmNames=[metricalarm["AlarmName"]])
+        if len(cluster_name) == 1:
+            if cluster_name[0]["Value"] not in RunningClusters:
                 CWclient.delete_alarms(AlarmNames=[metricalarm["AlarmName"]])
        
 AWS_Alarms()       
