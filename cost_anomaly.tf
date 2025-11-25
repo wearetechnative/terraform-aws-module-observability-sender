f0614957aa3c8201f0dd6d7941590d61be7d5d0f
jerpen80 <dra_penny@yahoo.com>
2025-11-25 14:19:25 +0100

    Added cost anomally

---
+++ cost_anomaly.tf
@@ -0,0 +1,26 @@
+resource "aws_ce_anomaly_monitor" "test" {
+  name              = "AWSServiceMonitor"
+  monitor_type      = "DIMENSIONAL"
+  monitor_dimension = "SERVICE"
+}
+
+resource "aws_cloudwatch_event_rule" "cost_anomaly" {
+  name        = "cost-anomaly-detection"
+  description = "Triggers when AWS Cost Anomaly Detection reports anomalies"
+
+  event_pattern = jsonencode({
+    "source" : ["aws.ce"],
+    "detail-type" : ["Cost Anomaly Detection Alert"],
+    "detail" : {
+        "impact" : {
+            "totalImpact" : [{ "numeric" : [">", 50] }]
+        }
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_target" "notification_receiver" {
+  rule      = aws_cloudwatch_event_rule.cost_anomaly.name
+  target_id = "notification_receiver"
+  arn       = aws_sns_topic.notification_receiver.arn
+}
